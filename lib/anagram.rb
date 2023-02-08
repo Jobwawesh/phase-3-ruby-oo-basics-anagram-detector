2418c316ebd81df85583982a2b525c70fd747636
Jobwawesh <jobnjunge15@gmail.com>
2023-02-08 17:57:42 +0300

    Completed Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+
+    def initialize (word)
+        @word = word
+    end
+
+    def match (list)
+        new_list = list.filter do |word|
+            @word.chars.sort == (word.chars).sort
+        end
+
+        return new_list
+    end
+    
+end
+listen = Anagram.new('listen')
+
